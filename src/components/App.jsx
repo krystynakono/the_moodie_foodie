829c234a1e7cd7c8da6391f223dea8446b92772c
Krystyna Malewski <krmalewski@gmail.com>
2016-12-06 16:17:57 -0500

    export app component

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import style from './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app">
+        Well hello there.
+      </div>
+    )
+  }
+}
+
+export default App;
